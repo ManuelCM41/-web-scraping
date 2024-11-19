bf61b40e284c1ad83b9fe986192f61ae352057db
ManuelCM41 <manuel.chunca@upeu.edu.pe>
2024-11-19 12:31:05 -0500

    API Reniec para validar DNI y otros

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -126,7 +126,7 @@
                             <!-- Item -->
                             <li class="flex px-2">
                                 <div
-                                    class="w-9 h-9 rounded-full shrink-0 bg-yellow-300 my-2 mr-3 text-white flex items-center justify-center">
+                                    class="w-9 h-9 rounded-full shrink-0 bg-yellow-400 my-2 mr-3 text-white flex items-center justify-center">
                                     <i class="fa-solid fa-building-columns fa-fw"></i>
                                 </div>
                                 <div class="grow flex items-center text-sm py-2">
@@ -151,23 +151,23 @@
                             <!-- Item -->
                             <li class="flex px-2">
                                 <div
-                                    class="w-9 h-9 rounded-full shrink-0 bg-yellow-600 my-2 mr-3 text-white flex items-center justify-center">
-                                    <i class="fa-solid fa-building-columns fa-fw"></i>
+                                    class="w-9 h-9 rounded-full shrink-0 bg-green-600 my-2 mr-3 text-white flex items-center justify-center">
+                                    <i class="fa-solid fa-newspaper fa-fw"></i>
                                 </div>
                                 <div class="grow flex items-center text-sm py-2">
                                     <div class="grow flex justify-between">
                                         <div class="self-center flex gap-1">
                                             En el diario <b>El Comercio</b> se escrapeo
                                             @if ($articles4Today->count() <= 2)
-                                            {{ $articles4Today->count() }}
+                                                {{ $articles4Today->count() }}
                                             @else
-                                            <x-counter-animation>{{ $articles4Today->count() }}</x-counter-animation>
+                                                <x-counter-animation>{{ $articles4Today->count() }}</x-counter-animation>
                                             @endif
                                             noticias en total
                                         </div>
                                         <div class="shrink-0 self-end ml-2">
                                             <a class="font-medium text-violet-500 hover:text-violet-600 dark:hover:text-violet-400"
-                                               href="#0">View<span class="hidden sm:inline"> -&gt;</span></a>
+                                                href="#0">View<span class="hidden sm:inline"> -&gt;</span></a>
                                         </div>
                                     </div>
                                 </div>
@@ -233,7 +233,7 @@
 
                             <li class="flex px-2">
                                 <div
-                                    class="w-9 h-9 rounded-full shrink-0 bg-yellow-300 my-2 mr-3 text-white flex items-center justify-center">
+                                    class="w-9 h-9 rounded-full shrink-0 bg-yellow-400 my-2 mr-3 text-white flex items-center justify-center">
                                     <i class="fa-solid fa-building-columns fa-fw"></i>
                                 </div>
                                 <div class="grow flex items-center text-sm py-2">
@@ -257,23 +257,23 @@
 
                             <li class="flex px-2">
                                 <div
-                                    class="w-9 h-9 rounded-full shrink-0 bg-yellow-600 my-2 mr-3 text-white flex items-center justify-center">
-                                    <i class="fa-solid fa-building-columns fa-fw"></i>
+                                    class="w-9 h-9 rounded-full shrink-0 bg-green-600 my-2 mr-3 text-white flex items-center justify-center">
+                                    <i class="fa-solid fa-newspaper fa-fw"></i>
                                 </div>
                                 <div class="grow flex items-center text-sm py-2">
                                     <div class="grow flex justify-between">
                                         <div class="self-center flex gap-1">
                                             En el diario <b>El Comercio</b> se obtuvo del escrapeo
                                             @if ($articles4Yesterday->count() <= 2)
-                                            {{ $articles4Yesterday->count() }}
+                                                {{ $articles4Yesterday->count() }}
                                             @else
-                                            <x-counter-animation>{{ $articles4Yesterday->count() }}</x-counter-animation>
+                                                <x-counter-animation>{{ $articles4Yesterday->count() }}</x-counter-animation>
                                             @endif
                                             noticias en total
                                         </div>
                                         <div class="shrink-0 self-end ml-2">
                                             <a class="font-medium text-violet-500 hover:text-violet-600 dark:hover:text-violet-400"
-                                               href="#0">View<span class="hidden sm:inline"> -&gt;</span></a>
+                                                href="#0">View<span class="hidden sm:inline"> -&gt;</span></a>
                                         </div>
                                     </div>
                                 </div>
@@ -335,13 +335,13 @@
                     'rgba(255, 99, 132, 0.5)',
                     'rgba(54, 162, 235, 0.5)',
                     'rgba(255, 205, 86, 0.5)',
-                    'rgba(255, 203, 5, 1)',
+                    'rgba(22, 163, 74, 0.5)',
                 ],
                 borderColor: [
                     'rgb(255, 99, 132)',
                     'rgb(54, 162, 235)',
                     'rgb(255, 205, 86)',
-                    'rgb(255, 203, 100)',
+                    'rgb(22, 163, 74)',
                 ],
                 borderWidth: 1
             }]
@@ -369,13 +369,13 @@
                     'rgba(255, 99, 132, 0.5)',
                     'rgba(54, 162, 235, 0.5)',
                     'rgba(255, 205, 86, 0.5)',
-                    'rgba(255, 203, 5, 1)',
+                    'rgba(22, 163, 74, 0.5)',
                 ],
                 borderColor: [
                     'rgb(255, 99, 132)',
                     'rgb(54, 162, 235)',
                     'rgb(255, 205, 86)',
-                    'rgb(255, 203, 100)',
+                    'rgb(22, 163, 74)',
                 ],
                 borderWidth: 1
             }]
